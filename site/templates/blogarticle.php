d9371507cda96234584f60a63131cae7f696e099
DerZyklop <mail@der-zyklop.de>
2013-12-13 02:51:59 +0100

    new headline style and some cleanup

--- site/templates/blogarticle.php
+++ site/templates/blogarticle.php
@@ -28,7 +28,6 @@
         <?php echo $page->parent()->author() ?>
       </div>
       <div class="right">
-          <div class="label">Spread love:</div>
           <a class="supporters-btn twitter" href="https://twitter.com/intent/tweet?text=<?php echo( urlencode( excerpt($page->title(), 100).' ' ) ) ?><?php echo($page->url()) ?><?php echo(urlencode(' (via @derzyklop)')) ?>">Twitter</a>
           <a class="supporters-btn facebook" href="http://www.facebook.com/share.php?u=<?php echo($page->url()) ?>&t=<?php echo( urlencode( $page->title() ) ) ?>">Facebook</a>
           <a class="supporters-btn flattr" href="https://flattr.com/submit/auto?user_id=DerZyklop&url=<?php echo($page->url()) ?>&title=<?php echo(urlencode($page->title())) ?>&description=<?php echo( urlencode(excerpt($page->text(), 260)) ) ?>&language=de_DE&<?php if ($page->tags()) : ?>tags=<?php echo( $page->tags() ) ?>&<?php endif; ?>category=text">Flattr</a>
