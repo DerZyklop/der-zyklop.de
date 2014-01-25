4a2569c9f910219677215a31734ff2d74529a3f7
DerZyklop <mail@der-zyklop.de>
2014-01-25 18:11:00 +0100

    added comments to new blogarticles

--- site/snippets/comments.list.php
+++ site/snippets/comments.list.php
@@ -1,10 +1,37 @@
 <?php
 
+
+function relative_time($date, $postfix = ' ago', $fallback = 'F Y')
+{
+    $diff = time() - strtotime($date);
+    if($diff < 60)
+        return $diff . ' second'. ($diff != 1 ? 's' : '') . $postfix;
+    $diff = round($diff/60);
+    if($diff < 60)
+        return $diff . ' minute'. ($diff != 1 ? 's' : '') . $postfix;
+    $diff = round($diff/60);
+    if($diff < 24)
+        return $diff . ' hour'. ($diff != 1 ? 's' : '') . $postfix;
+    $diff = round($diff/24);
+    if($diff < 7)
+        return $diff . ' day'. ($diff != 1 ? 's' : '') . $postfix;
+    $diff = round($diff/7);
+    if($diff < 4)
+        return $diff . ' week'. ($diff != 1 ? 's' : '') . $postfix;
+    $diff = round($diff/4);
+    if($diff < 12)
+        return $diff . ' month'. ($diff != 1 ? 's' : '') . $postfix;
+
+    return date($fallback, strtotime($date));
+};
+
+
+
 require 'comments.read.php';
 
 if (count($comments['data'])):
 ?>
-	<h6><span><?= l::get('comments.title') ?: 'Kommentare' ?></span></h6>
+	<h6><span><?= count($comments['data']) ?> <?= l::get('comments.title') ?: 'Kommentare' ?></span></h6>
 	<ul id="comments">
 		<?php $index = 0; ?>
 		<?php foreach ($comments['data'] as $comment):
@@ -13,12 +40,13 @@ if (count($comments['data'])):
 			};
 			$index = $index+1;
 			echo '<li class="'.(( $index % 2 == 0 ) ? 'odd' : 'even').'">';
-			echo (c::get('comments.gravatar') ? '<div class="comment-gravatar">'.$comment['gravatar'].'</div>' : '').
-			'
-			<div class="comment-name">'.$comment['name'].'</div>
-			<div class="comment-date">'.date('d.m.Y', strtotime($comment['date'])).'</div>
-			<div class="clearit"></div>';
-			echo '<div class="comment-text">'.kirbytext($comment['text']).'</div>
+				echo (c::get('comments.gravatar') ? '<div class="comment-gravatar">'.$comment['gravatar'].'</div>' : '');
+				echo '<div class="comment-name">'.$comment['name'].'</div>';
+				echo '<div class="comment-date">'.relative_time($comment['date']).'</div>';
+				#echo '<div class="comment-date">'.date('H:m d.m.Y', strtotime($comment['date'])).'</div>';
+
+				echo '<div class="clearit"></div>';
+				echo '<div class="comment-text">'.kirbytext($comment['text']).'</div>
 			</li>';
 		endforeach; ?>
 	</ul>
