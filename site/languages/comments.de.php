8f7a787733b8c934d4059c24b30eb8020740ad90
DerZyklop <mail@der-zyklop.de>
2014-01-26 15:26:00 +0100

    somehow the translations doesn't work. had to add translations to the config.php – dirty.. i know.

---
+++ site/languages/comments.de.php
@@ -0,0 +1,12 @@
+<?php
+
+l::set('comments.title', 'Kommentare');
+l::set('comments.add', 'Kommentar schreiben');
+l::set('comments.name', 'Name');
+l::set('comments.email', 'Email');
+l::set('comments.text', 'Kommentar');
+l::set('comments.send', 'Senden');
+l::set('comments.file_error', 'Fehler 42. Wenn du mir einen gefallen tun willst, schickst du das an mail@der-zyklop.de');
+l::set('comments.saved', 'Danke! Ich freu mich über jeden kommentar!\nGrüße, Nils');
+
+?>
