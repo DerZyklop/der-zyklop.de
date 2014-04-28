bce62042b0e1697db662df0064c0b2c2f0e53ac3
DerZyklop <mail@der-zyklop.de>
2014-04-28 11:34:09 +0200

    added starterkit by getkirby to Submodules

---
+++ submodules/starterkit/site/blueprints/site.php
@@ -0,0 +1,20 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Site
+subpages: true
+fields:
+  title: 
+    label: Title
+    type:  text
+  author: 
+    label: Author
+    type:  text
+  keywords: 
+    label: Keywords
+    type:  tags
+  description: 
+    label: Description
+    type:  textarea
+  copyright: 
+    label: Copyright
+    type:  text
