4e4fd8ecff6d0bd4b91f8f91e08d2ca15da98f4e
DerZyklop <mail@der-zyklop.de>
2014-06-08 16:39:09 +0200

    added user

---
+++ site/accounts/DerZyklop.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: DerZyklop
+email: mail@der-zyklop.de
+password: >
+  $2a$10$u2TP4vlt8F6LBoyOVzMkG.eev1UMkGBo9dvVRyCvjbxPknKDO8.SW
+language: en
+token: 6cbb2cb64c59b39479c649cceacdbb8b036c3c05
