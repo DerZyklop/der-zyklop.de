54d846c6a503990bb2bab98c63e736cfb2b51696
DerZyklop <mail@der-zyklop.de>
2014-06-09 12:40:14 +0200

    new article #81

--- site/accounts/DerZyklop.php
+++ site/accounts/DerZyklop.php
@@ -5,4 +5,4 @@ email: mail@der-zyklop.de
 password: >
   $2a$10$u2TP4vlt8F6LBoyOVzMkG.eev1UMkGBo9dvVRyCvjbxPknKDO8.SW
 language: en
-token: 9656c66faf6f1c30d772ada77b910b71364b80f4
+token: 455b8815603f3175ab7b4bb3df5eb0a29703daa3
