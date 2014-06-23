d03b8a1bb1db7049a31a34dec98325fe63257975
DerZyklop <mail@der-zyklop.de>
2014-06-23 19:08:24 +0200

    new article #85 and #86

--- site/accounts/DerZyklop.php
+++ site/accounts/DerZyklop.php
@@ -5,4 +5,4 @@ email: mail@der-zyklop.de
 password: >
   $2a$10$u2TP4vlt8F6LBoyOVzMkG.eev1UMkGBo9dvVRyCvjbxPknKDO8.SW
 language: en
-token: a863179d880667c6b311d8f3ad03e451d496d5cc
+token: e98b0d67524d2f662e9f77073b3fec3e173f3312
--- site/accounts/elishes.php
+++ site/accounts/elishes.php
@@ -5,3 +5,4 @@ username: elishes
 email: elisa-heuser@gmx.de
 password: >
   $2a$10$azqoMdrImRTiGj.CWSX.KOkKQTrHnd6TKpiMgTIX3V5QntRchw.0q
+token: 4cfec35655384510ae8716b121a66c0ae3f0652e
