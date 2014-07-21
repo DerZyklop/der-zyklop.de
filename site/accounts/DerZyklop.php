80e0b2306aaaa9323cbdd2bc0e8d48c3783f0776
DerZyklop <mail@der-zyklop.de>
2014-07-21 17:06:28 +0200

    added new account

--- site/accounts/DerZyklop.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: derzyklop
-email: mail@der-zyklop.de
-password: >
-  $2a$10$XzNYIF5r17.zd9lYjFW7FOcjFdr1I4814Cq7k.xbqAvPxpmi2R0bO
-language: en
-token: e2fea377788b27b3569346eb1ed039b548f41282
---
+++ site/accounts/derzyklop.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: derzyklop
+email: mail@der-zyklop.de
+password: >
+  $2a$10$xfC0pznHQKVcxlnXiORG5.NV7w8mrOcmyG5Y/j6nctxMMELBsuhGW
+language: en
+token: d348bb66342a9df311527bdbee7a2eb2785c0ef0
