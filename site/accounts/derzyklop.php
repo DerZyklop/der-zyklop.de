bdb4d0ead7df42a04e47363892aa4ce9c726688d
DerZyklop <mail@der-zyklop.de>
2014-08-11 09:38:08 +0200

    new article 94

--- site/accounts/derzyklop.php
+++ site/accounts/derzyklop.php
@@ -5,4 +5,4 @@ email: mail@der-zyklop.de
 password: >
   $2a$10$xfC0pznHQKVcxlnXiORG5.NV7w8mrOcmyG5Y/j6nctxMMELBsuhGW
 language: en
-token: d348bb66342a9df311527bdbee7a2eb2785c0ef0
+token: d5438bfc302258c3df5602d0577b38a00772b9bb
