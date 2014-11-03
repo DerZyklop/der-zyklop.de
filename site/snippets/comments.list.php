30d378632dd5e3a7736ee70eddb605f1640cfa67
DerZyklop <mail@der-zyklop.de>
2014-11-03 18:36:36 +0100

    new comments version

--- site/snippets/comments.list.php
+++ site/snippets/comments.list.php
@@ -29,46 +29,48 @@ function relative_time($date, $postfix = ' ago', $fallback = 'F Y')
 
 require 'comments.read.php';
 ?>
-<div class="comments-list">
-  <?php if (count($comments['data']) != 0) : ?>
-    <h6><span><?php
-      echo count($comments['data']).' ';
-      if (count($comments['data']) == 0) {
-        echo l::get('comments.title') ?: 'Kommentare';
-      } elseif (count($comments['data']) == 1) {
-        echo 'Kommentar';
-      } else {
-        echo l::get('comments.title') ?: 'Kommentare';
-      }
+<?php if (count($comments['data']) != 0) : ?>
+<section class="width-wrap">
+  <div class="comments-list">
+    <?php if (count($comments['data']) != 0) : ?>
+      <h6><span><?php
+        echo count($comments['data']).' ';
+        if (count($comments['data']) == 0) {
+          echo l::get('comments.title') ?: 'Kommentare';
+        } elseif (count($comments['data']) == 1) {
+          echo 'Kommentar';
+        } else {
+          echo l::get('comments.title') ?: 'Kommentare';
+        }
 
-    ?></span></h6>
+      ?></span></h6>
+    <?php endif; ?>
+  <?php
+  if (count($comments['data'])):
+  ?>
+    <ul id="comments">
+      <?php $index = 0; ?>
+      <?php foreach ($comments['data'] as $comment):
+        $index = $index+1;
+        echo '<li class="'.(( $index % 2 == 0 ) ? 'odd' : 'even').'">';
+      ?>
+        <?php $anchor = date("Ymd_his",strtotime($comment['date'])) ?>
+        <a class="comment-anchor" href="#<?= $anchor ?>" id="<?= $anchor ?>">
+          <div class="comment-name"><?= $comment['name'] ?></div>
+          <div class="comment-date"><?= relative_time($comment['date']) ?></div>
+          <div class="clearit"></div>
+        </a>
+      <?php
+          echo (c::get('comments.gravatar') ? '<div class="comment-gravatar"><div><a href="https://de.gravatar.com/">'.$comment['gravatar'].'</a></div></div>' : '');
+          #echo '<div class="comment-date">'.date('H:m d.m.Y', strtotime($comment['date'])).'</div>';
+
+          echo '<div class="clearit"></div>';
+          echo '<div class="comment-text">'.kirbytext($comment['text']).'</div>
+        </li>';
+      endforeach; ?>
+    </ul>
   <?php endif; ?>
-<?php
-if (count($comments['data'])):
-?>
-  <ul id="comments">
-    <?php $index = 0; ?>
-    <?php foreach ($comments['data'] as $comment):
-      $index = $index+1;
-      echo '<li class="'.(( $index % 2 == 0 ) ? 'odd' : 'even').'">';
-    ?>
-      <?php $anchor = date("Ymd_his",strtotime($comment['date'])) ?>
-      <a class="comment-anchor" href="#<?= $anchor ?>" id="<?= $anchor ?>">
-        <div class="comment-name"><?= $comment['name'] ?></div>
-        <div class="comment-date"><?= relative_time($comment['date']) ?></div>
-        <div class="clearit"></div>
-      </a>
-    <?php
-        echo (c::get('comments.gravatar') ? '<div class="comment-gravatar"><div><a href="https://de.gravatar.com/">'.$comment['gravatar'].'</a></div></div>' : '');
-        #echo '<div class="comment-date">'.date('H:m d.m.Y', strtotime($comment['date'])).'</div>';
-
-        echo '<div class="clearit"></div>';
-        echo '<div class="comment-text">'.kirbytext($comment['text']).'</div>
-      </li>';
-    endforeach; ?>
-  </ul>
-<?php
-endif;
-?>
-</div>
+  </div>
+<?php endif; ?>
+</section>
 <?php
