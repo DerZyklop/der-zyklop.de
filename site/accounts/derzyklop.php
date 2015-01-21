3d711bf8c03fb4554a0575bdf9a9d2c2f81ca820
DerZyklop <mail@der-zyklop.de>
2015-01-21 13:43:56 +0100

    added new drafts

--- site/accounts/derzyklop.php
+++ site/accounts/derzyklop.php
@@ -5,12 +5,10 @@ email: mail@der-zyklop.de
 password: >
   $2a$10$xfC0pznHQKVcxlnXiORG5.NV7w8mrOcmyG5Y/j6nctxMMELBsuhGW
 language: en
-token: 04dbda547088b729d68afec9c61813d372a6a3c9
+token: 1e9cdb94b03d74d0e2dec119d59847439eb24c18
 history:
-  - blog/example-blogarticle
-  - blog/marke
-  - >
-    blog/bin-auf-der-suche-nach-einem-guten-newsletter-system-fuer-meinen-blog
-  - >
-    blog/besseres-javascript-mit-linting-und-coffeescript
-  - blog/apples-new-transparent-backgrounds
+  - impressum
+  - blog/command-line-alias-doch-sudo
+  - blog/npm-updates-alle-auf-einmal-machen
+  - blog/wochenrueckblick-3
+  - blog/jan-boehmermann
