f2c0ab9bc32c8b4324405b403264dc72ceabc6bd
DerZyklop <mail@der-zyklop.de>
2015-03-15 14:47:40 +0100

    removed unneccessary sites

--- site/accounts/derzyklop.php
+++ site/accounts/derzyklop.php
@@ -5,10 +5,10 @@ email: mail@der-zyklop.de
 password: >
   $2a$10$xfC0pznHQKVcxlnXiORG5.NV7w8mrOcmyG5Y/j6nctxMMELBsuhGW
 language: en
-token: 1e9cdb94b03d74d0e2dec119d59847439eb24c18
+token: 1b8346b8c444ea34b9a8fd8e5a6705f11cf996cf
 history:
-  - impressum
-  - blog/command-line-alias-doch-sudo
-  - blog/npm-updates-alle-auf-einmal-machen
-  - blog/wochenrueckblick-3
-  - blog/jan-boehmermann
+  - blog/derzyklop-ist-jetzt-mainzer
+  - blog/wochenrueckblick-9
+  - blog/minimalismus-stammtisch-koeln
+  - about
+  - blog/wochenrueckblick-8
