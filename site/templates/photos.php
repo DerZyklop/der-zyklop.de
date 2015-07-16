83b9cb9b6ccd30c8ab9b99e80fe4c6fa4042bc91
Nils Neumann <neumann@placeworkers.com>
2015-07-16 18:04:42 +0200

    git reset

---
+++ site/templates/photos.php
@@ -0,0 +1,11 @@
+<?php snippet('header'); ?>
+<?php snippet('dz-banner-small') ?>
+
+<div class="page-wrap">
+  <div class="content">
+
+    <?php snippet('500px') ?>
+
+  </div>
+
+<?php snippet('footer') ?>
