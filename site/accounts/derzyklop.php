ab48cdcb732283e2b925d327deb6ea24b00a606c
DerZyklop <mail@der-zyklop.de>
2015-09-01 15:12:19 +0200

    added article 167,168,169

--- site/accounts/derzyklop.php
+++ site/accounts/derzyklop.php
@@ -5,10 +5,12 @@ email: mail@der-zyklop.de
 password: >
   $2a$10$xfC0pznHQKVcxlnXiORG5.NV7w8mrOcmyG5Y/j6nctxMMELBsuhGW
 language: en
-token: 059bc27a8830a89f9517dc14bf48d88d05d3857c
+token: 7023328af8b63a585b3c64a9e4e2fbe3fbafd5cb
 history:
-  - blog/bucket-list-challange
-  - blog/minimalismus-blogs
-  - blog/derzyklop-ist-jetzt-mainzer
-  - blog/wochenrueckblick-9
-  - blog/minimalismus-stammtisch-koeln
+  - >
+    blog/facebook-hat-meinen-account-gesperrt
+  - >
+    blog/fix-bluetooth-issues-after-sleep-mbp
+  - blog/stimme-aus-dem-off
+  - blog/terminal-ip-country
+  - blog/neue-fotos-auf-500px-com
