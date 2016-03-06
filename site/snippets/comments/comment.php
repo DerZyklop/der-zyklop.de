0b56986c587c9c798a5a20ff7c1708ac31fe180d
DerZyklop <mail@der-zyklop.de>
2016-03-06 15:10:31 +0100

    finished comments plugin

--- site/snippets/comments/comment.php
+++ site/snippets/comments/comment.php
@@ -2,24 +2,30 @@
 
   <div class="comment-meta">
 
-    <div class="comment-author vcard">
-      <?php echo $comment->gravatar(); ?>
-      <a href="mailto:<?php echo $comment->authorEmail()->obfuscate(); ?>" class="fn">
-        <?php echo $comment->author(); ?>
-      </a>
-    </div>
-
     <time datetime="<?php echo $comment->date('c'); ?>">
-      <?php echo $comment->date('d.m.Y H:i'); ?>
+      <h6 style="text-align: right" class="entry-date">
+        <span>
+          <?php echo $comment->date('d.m.Y H:i'); ?>
+        </span>
+      </h6>
     </time>
 
-    <?php echo $comment->editLink(); ?>
-    <?php echo $comment->deleteLink(); ?>
+    <div class="comment-author vcard">
+      <?php echo $comment->gravatar(); ?>
+      <h5>
+        <a href="mailto:<?php echo $comment->authorEmail()->obfuscate(); ?>" class="fn">
+          <?php echo $comment->author(); ?>
+        </a>
+      </h5>
+    </div>
 
   </div>
 
   <div class="comment-content">
     <?php echo $comment->text()->safeMarkdown( c::get('comments.markdown') ); ?>
   </div>
-
+  <p>
+    <?php echo $comment->editLink(); ?>
+    <?php echo $comment->deleteLink(); ?>
+  </p>
 </article>
