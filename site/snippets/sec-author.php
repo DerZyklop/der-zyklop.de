d2dd2f96297e323ea9c4f88146be81687f7582ab
DerZyklop <mail@der-zyklop.de>
2016-03-06 17:51:30 +0100

    fixed author css issue

--- site/snippets/sec-author.php
+++ site/snippets/sec-author.php
@@ -2,7 +2,7 @@
   <section class="width-wrap">
     <h6><span>Autor</span></h6>
     <img class="right" style="margin: 0 0 1em 1em" height="60" width="60" src="<?= url('assets/images/derzyklop_avatar.png') ?>">
-    <h5 style="text-align: left;margin:2em 0 0"><strong><a href="/about"><?= $site->author() ?></a></strong></h5>
+    <h4 style="text-align: left;margin:2em 0 0"><strong><a href="/about"><?= $site->author() ?></a></strong></h4>
     <?= kirbytext($site->authordescription()) ?>
   </section>
   <div class="clearit"></div>
