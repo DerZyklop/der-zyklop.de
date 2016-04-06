0cad46ca4b45a7fc436dcffebd059578f9211904
DerZyklop <mail@der-zyklop.de>
2016-04-07 00:12:15 +0200

    improved styles for author secion

--- site/snippets/sec-author.php
+++ site/snippets/sec-author.php
@@ -1,8 +1,11 @@
 <div class="author bg-tertiary">
   <section>
     <h6><span>Autor</span></h6>
-    <img class="left" style="margin: 0.5em 2em 1em 1em" height="160" src="<?= url('assets/images/500pxch2015tie.jpg') ?>">
-    <div style="padding: 1px 0">
+    <div class="flex">
+      <img
+        height="160"
+        src="<?= url('assets/images/500pxch2015tie.jpg') ?>"
+      >
       <?= kirbytext($site->authordescription()) ?>
     </div>
   </section>
