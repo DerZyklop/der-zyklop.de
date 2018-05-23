bfec5330fab43cf21e674135e675c1313b251650
DerZyklop <mail@der-zyklop.de>
2018-05-23 20:35:49 +0200

    content changes

--- site/accounts/derzyklop.php
+++ site/accounts/derzyklop.php
@@ -6,9 +6,9 @@ password: >
   $2a$10$xfC0pznHQKVcxlnXiORG5.NV7w8mrOcmyG5Y/j6nctxMMELBsuhGW
 language: en
 history:
-  - error
-  - blog/schoener-wohnen
-  - blog/wifi-in-giessener-stadtbussen
-  - blog/mac-apps-zur-netzwerk-analyse
-  - about
+  - blog/mallorca-ohne-longboard
+  - blog/minimalismus-blogs
+  - blog/meditation
+  - blog/das-allrad-allmende-lastenrad-projekt
+  - blog/der-richtige-espressokocher-fuer-den-herd
 token: ee7a73d993b944388b02d51cf1223af108817f4d
