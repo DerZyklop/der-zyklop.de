ea64a73db062e8e902075e6f8dee1d3abecbe0db
DerZyklop <mail@der-zyklop.de>
2018-05-23 21:38:27 +0200

    no-referrer

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,6 +3,7 @@
 <html lang="de-DE">
 <head>
   <meta charset="UTF-8">
+  <meta name="referrer" content="no-referrer">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= html($page->title()) ?> - <?= html($site->title()) ?></title>
 
